2ad17646199c3044ec65f1a131e97be47948488a
q15004040209-creator <q15004040209@gmail.com>
2026-06-09 20:18:39 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="httpx_wrap",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Python 下一代 HTTP 客户端，支持异步",
+    python_requires=">=3.8",
+)
